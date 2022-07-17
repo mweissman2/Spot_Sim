a1d5d5e889c780e92b021b2e615deaba29a08a94
mweissman2 <94194131+mweissman2@users.noreply.github.com>
2022-07-17 14:50:51 -0400

    Working foot_pos.py

--- data_acq/src/foot_pos.py
+++ data_acq/src/foot_pos.py
@@ -9,6 +9,7 @@ from   std_msgs.msg       import Float64
 from   geometry_msgs.msg  import Twist
 from   visualization_msgs.msg  import MarkerArray
 from   visualization_msgs.msg  import Marker
+import csv
 
 
 f1_x = 0.0
@@ -26,6 +27,12 @@ f3_z = 0.0
 f4_x = 0.0
 f4_y= 0.0
 f4_z = 0.0
+
+header = ['f1_x', 'f1_y', 'f1_z', 'f2_x', 'f2_y', 'f2_z', 'f3_x', 'f3_y', 'f3_z', 'f4_x', 'f4_y', 'f4_z']
+
+f = open('src/Spot_Sim/data_acq/src/pos.csv', 'w')
+writer = csv.writer(f)
+writer.writerow(header)
 
 def get_position(data1):
     global f1_x, f1_y, f1_z, f2_x, f2_y, f2_z, f3_x, f3_y, f3_z, f4_x, f4_y, f4_z
@@ -48,38 +55,60 @@ def get_position(data1):
     f4_y = markers[3].pose.position.y
     f4_z = markers[3].pose.position.z
 
+    print('Foot 1 x: = {}'.format(f1_x))
+    print('Foot 1 y: = {}'.format(f1_y))
+    print('Foot 1 z: = {}'.format(f1_z))
+    print('Foot 2 x: = {}'.format(f2_x))
+    print('Foot 2 y: = {}'.format(f2_y))
+    print('Foot 2 z: = {}'.format(f2_z))
+    print('Foot 3 x: = {}'.format(f3_x))
+    print('Foot 3 y: = {}'.format(f3_y))
+    print('Foot 3 z: = {}'.format(f3_z))
+    print('Foot 4 x: = {}'.format(f4_x))
+    print('Foot 4 y: = {}'.format(f4_y))
+    print('Foot 4 z: = {}'.format(f4_z))
+
+    # if writer is not None:
+    writer.writerow([f1_x, f1_y, f1_z, f2_x, f2_y, f2_z, f3_x, f3_y, f3_z, f4_x, f4_y, f4_z])
+
 
 def main():
 
     global f1_x, f1_y, f1_z, f2_x, f2_y, f2_z, f3_x, f3_y, f3_z, f4_x, f4_y, f4_z
 
 
-    rospy.init_node('gimbal_align', anonymous=True)
+    rospy.init_node('data_acq', anonymous=True)
     rate = rospy.Rate(5) # 5hz
 
     rospy.loginfo("This Node is working")
 
     sub = rospy.Subscriber('/foot', MarkerArray, get_position)
-
-
-
-    while not rospy.is_shutdown():
+    
         
-        print('Foot 1 x: = {}'.format(f1_x))
-        print('Foot 1 y: = {}'.format(f1_y))
-        print('Foot 1 z: = {}'.format(f1_z))
-        print('Foot 2 x: = {}'.format(f2_x))
-        print('Foot 2 y: = {}'.format(f2_y))
-        print('Foot 2 z: = {}'.format(f2_z))
-        print('Foot 3 x: = {}'.format(f3_x))
-        print('Foot 3 y: = {}'.format(f3_y))
-        print('Foot 3 z: = {}'.format(f3_z))
-        print('Foot 4 x: = {}'.format(f4_x))
-        print('Foot 4 y: = {}'.format(f4_y))
-        print('Foot 4 z: = {}'.format(f4_z))
-    	
-        rate.sleep()
-    # rospy.spin()
+
+
+    
+
+    # while not rospy.is_shutdown():
+        
+        # print('Foot 1 x: = {}'.format(f1_x))
+        # print('Foot 1 y: = {}'.format(f1_y))
+        # print('Foot 1 z: = {}'.format(f1_z))
+        # print('Foot 2 x: = {}'.format(f2_x))
+        # print('Foot 2 y: = {}'.format(f2_y))
+        # print('Foot 2 z: = {}'.format(f2_z))
+        # print('Foot 3 x: = {}'.format(f3_x))
+        # print('Foot 3 y: = {}'.format(f3_y))
+        # print('Foot 3 z: = {}'.format(f3_z))
+        # print('Foot 4 x: = {}'.format(f4_x))
+        # print('Foot 4 y: = {}'.format(f4_y))
+        # print('Foot 4 z: = {}'.format(f4_z))
+        
+        # rate.sleep()
+    rospy.spin()
+    f.close()
+    csv_writer = None
+
 
 if __name__ == '__main__':
     main()
